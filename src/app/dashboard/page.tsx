6f3ae6c085404a6671d6dfd99ca0ca229b2ed0c6
Meh Chow <mehchow0604@gmail.com>
2025-06-03 17:12:01 +0800

    fix dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-
-import authApi from "@/services/auth-api";
-import { useEffect } from "react";
-
-export function DashboardPage() {
-  const checkAuth = async () => {
-    const response = await authApi.getUser();
-    console.log(response);
-  };
-
-  useEffect(() => {
-    checkAuth();
-  }, []);
-
-  return <div className="flex items-center justify-center h-screen"></div>;
+export default function DashboardPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>Dashboard page. Temp</h1>
+    </div>
+  );
 }
-
-export default DashboardPage;
