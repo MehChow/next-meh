35052271a676050d0fe1f428fe7cb7f0d6691cb8
Meh Chow <mehchow0604@gmail.com>
2025-06-03 17:21:25 +0800

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-export default function DashboardPage() {
+export default function Dashboard() {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1>Dashboard page. Temp</h1>
