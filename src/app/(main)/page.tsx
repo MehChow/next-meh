b85af09d45512f219235fc52b459b81b7788bfdb
Meh Chow <mehchow0604@gmail.com>
2025-06-04 13:39:46 +0800

    changed title; give up deploy backend

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <main className="max-w-[2400px] w-full"></main>;
+  return (
+    <main className="max-w-[2400px] w-full">
+      <div className="flex items-center justify-center bg-red-100">
+        <h1 className="text-4xl font-bold">Welcome to Animal Yapping</h1>
+      </div>
+    </main>
+  );
 }
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,10 +10,10 @@ export function Logo({ color, paddingTop }: LogoProps) {
   return (
     <Link
       href="/"
-      className={`text-4xl font-bold flex items-center gap-4 absolute left-0 pl-6 z-99 ${color} ${paddingTop}`}
+      className={`text-3xl font-bold flex items-center gap-4 absolute left-0 pl-6 z-99 ${color} ${paddingTop}`}
     >
       <Image src="/logo.png" alt="Logo" width={75} height={75} quality={100} />
-      Dynamic
+      Animal Yapping
     </Link>
   );
 }
